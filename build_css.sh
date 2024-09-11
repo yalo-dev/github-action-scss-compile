35ed318990639d0e9a1c0476c2e025a7aa17df2a
Chris Dawson <chris.dawson@digitalyalo.com>
2024-09-11 13:53:50 -0400

    update script to use args

--- build_css.sh
+++ build_css.sh
@@ -1,16 +1,9 @@
 #!/bin/sh -l
 
-set -e
-
-: ${source?Required source path not set.}
-: ${destination?Required destination path not set.}
-
-echo "Source and Destination found; Building SCSS to CSS"
-
 echo "Installing Sass"
 npm install -g sass
 
 echo "Build SCSS"
-sass $source $destination
+sass $1 $2
 
 ls -a
