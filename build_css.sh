de22ee8747b9398ef6798f957bd8a820343462c1
Chris Dawson <chrisdawson006@gmail.com>
2025-06-04 16:38:49 -0400

    rm node_modules directory after deploy

--- build_css.sh
+++ build_css.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
-echo "NPM Install"
+echo "npm nstall"
 npm install
 
-echo "Build Gulp"
+echo "npm run build"
 npm run build
 
 ls -a
+
+echo "removing node_modules to speed up deploy"
+rm -rf node_modules/
